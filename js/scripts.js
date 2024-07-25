e120e33c75acd2260a9f73f7d77f2d034fb5583f
mattiapa <mattia.papa95@gmail.com>
2024-07-25 17:48:11 +0200

    merging & background image + row layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
-const navToggle = document.querySelector('.nav-toggle')
-const mainNav = document.querySelector('.main-nav')
-const hamburger = document.querySelector('.hamburger')
+// const navToggle = document.querySelector('.nav-toggle')
+// const mainNav = document.querySelector('.main-nav')
+// const hamburger = document.querySelector('.hamburger')
 
-navToggle.addEventListener("click", () => {
+// navToggle.addEventListener("click", () => {
 
-    mainNav.toggleAttribute('is-open')
-    hamburger.toggleAttribute('is-open')
-    navToggle.toggleAttribute('is-open')
-})
+//     mainNav.toggleAttribute('is-open')
+//     hamburger.toggleAttribute('is-open')
+//     navToggle.toggleAttribute('is-open')
+// })
+
+// scrolling menu
+// const nav = document.querySelector(".menu-bar");
+// console.log("query selector: ", document.querySelectorAll(".header"))
+// let lastScrollY = window.scrollY;
+// window.addEventListener("scroll", () => {
+//     if (lastScrollY < window.scrollY) {
+//         console.log("we are going down");
+//         nav.classList.add("nav--hidden")
+//     } else {
+//         console.log("we are going up");
+//         nav.classList.remove("nav--hidden")
+//     }
+
+//     lastScrollY = window.scrollY;
+// });
