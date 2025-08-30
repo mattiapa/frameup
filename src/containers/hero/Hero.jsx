6c898fbb63fd90759ac53f73b35e2c244afb2ead
gaf1990 <ferraro.giuseppe.andrea@gmail.com>
2025-08-31 00:55:16 +0200

    Add new images for hero page, fix Hero Text positioning

--- src/containers/hero/Hero.jsx
+++ src/containers/hero/Hero.jsx
@@ -1,27 +1,29 @@
 import './hero.css'
-import { Button, Icons } from '../../components'
+import {Button, Icons} from '../../components'
 import React from "react";
 
 const Hero = () => {
-  return (
-    <div className="hero-image">
-      <div className='section hero-section' id="hero">
-        {/* <h1 className='gradient__text hero-title'>Frame Up</h1> */}
-        <h1 className='hero-title'>Frame Up</h1>
-        <Button
-          buttonText={'ascolta ora'}
-          type='btn-primary'
-          href={"https://www.youtube.com/channel/UCF2Dosdhyba_x8g4ChmMBmQ"}
-        />
-       <div className="hero-bottom">
-        <p>Segui. Condividi. Ascolta.</p>
-        <div>
-          <Icons className="hero-icons"/>
+    return (
+        <div className="hero-image">
+            <div className='section hero-section' id="hero">
+                <div className="hero-elements">
+                    {/* <h1 className='gradient__text hero-title'>Frame Up</h1> */}
+                    <h1 className='hero-title'>Frame Up</h1>
+                    <Button
+                        buttonText={'ascolta ora'}
+                        type='btn-primary'
+                        href={"https://www.youtube.com/channel/UCF2Dosdhyba_x8g4ChmMBmQ"}
+                    />
+                    <div className="hero-bottom">
+                        <p>Segui. Condividi. Ascolta.</p>
+                        <div>
+                            <Icons className="hero-icons"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-       </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Hero
